4895c9a6ac1fe1f053ce1662aff161384856d05b
Bducrocq <bducrocq42@gmail.com>
2022-06-08 16:07:26 +0200

    fork mutex ok

--- srcs/routines.c
+++ srcs/routines.c
@@ -6,7 +6,7 @@
 /*   By: bducrocq <bducrocq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 18:22:31 by bducrocq          #+#    #+#             */
-/*   Updated: 2022/06/07 16:09:28 by bducrocq         ###   ########.fr       */
+/*   Updated: 2022/06/08 15:43:49 by bducrocq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,50 @@ int	philo_taken_fork(t_data *data, int id)
 	return(0);
 }
 
-int	philo_eating(t_data *data, int id)
+int	last_philo_eating(t_data *data, int id)
 {
 	pthread_mutex_lock(&data->forks[id - 1].mtx_forks);
 	data->forks[id - 1].availability = FORK_NOT_AVAILABLE;
-	print_pstate_change(STATE_FORK, id, data->philos->tid, data, id - 1);
-
-	pthread_mutex_lock(&data->forks[id].mtx_forks);
+	print_pstate_change(STATE_FORK, id, data->philos->tid, data, \
+		id - 1);
+	
+	pthread_mutex_lock(&data->forks[0].mtx_forks);
 	data->forks[id].availability = FORK_NOT_AVAILABLE;
-	print_pstate_change(STATE_FORK, id, data->philos->tid, data, id);
+	print_pstate_change(STATE_FORK, id, data->philos->tid, data, 0);
 
 	print_pstate_change(STATE_EATING, id, data->philos->tid, data, 0);
 	usleep(data->time_rules.time_to_eat);
 	
+	data->forks[0].availability = FORK_AVAILABLE;
 	data->forks[id - 1].availability = FORK_AVAILABLE;
-	data->forks[id].availability = FORK_AVAILABLE;
+	pthread_mutex_unlock(&data->forks[0].mtx_forks);
 	pthread_mutex_unlock(&data->forks[id - 1].mtx_forks);
-	pthread_mutex_unlock(&data->forks[id].mtx_forks);
+
+	return(0);
+}
+
+int	philo_eating(t_data *data, int id)
+{
+	if (id == data->number_of_philo)
+		last_philo_eating(data, id);
+	else
+	{
+		pthread_mutex_lock(&data->forks[id - 1].mtx_forks);
+		data->forks[id - 1].availability = FORK_NOT_AVAILABLE;
+		print_pstate_change(STATE_FORK, id, data->philos->tid, data, id - 1);
+
+		pthread_mutex_lock(&data->forks[id].mtx_forks);
+		data->forks[id].availability = FORK_NOT_AVAILABLE;
+		print_pstate_change(STATE_FORK, id, data->philos->tid, data, id);
+
+		print_pstate_change(STATE_EATING, id, data->philos->tid, data, 0);
+		usleep(data->time_rules.time_to_eat);
+
+		data->forks[id - 1].availability = FORK_AVAILABLE;
+		data->forks[id].availability = FORK_AVAILABLE;
+		pthread_mutex_unlock(&data->forks[id - 1].mtx_forks);
+		pthread_mutex_unlock(&data->forks[id].mtx_forks);
+	}
 
 	return(0);
 }
