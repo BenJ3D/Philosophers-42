2b0b224be877342c24680c442701c05f71d3fd2e
Bducrocq <bducrocq42@gmail.com>
2022-06-24 19:08:40 +0200

    save

--- srcs/philo.c
+++ srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: bducrocq <bducrocq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 16:07:14 by bducrocq          #+#    #+#             */
-/*   Updated: 2022/06/24 18:55:58 by bducrocq         ###   ########.fr       */
+/*   Updated: 2022/06/24 19:05:36 by bducrocq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ int	init_philo(t_data *data)
 
 	i = 0;
 	data->id_philo = 0;
-	while(i < data->number_of_philo - 1)
+	while(i < data->number_of_philo)
 	{
 		data->id_philo++;
 		if(pthread_create(&data->philos[i].tid, NULL, &philo_routine, data))
@@ -57,7 +57,7 @@ int	init_philo(t_data *data)
 		if (data->time_rules.ate_max_imposed == TRUE)
 			data->philos[i].ate_max = data->time_rules.max_philo_must_eat;
 		i++;
-		usleep(60); /////////////////////////////// INTERVAL BORN PHILO
+		usleep(75); /////////////////////////////// INTERVAL BORN PHILO
 	}
 	while (data->id_philo > 0)
 	{
